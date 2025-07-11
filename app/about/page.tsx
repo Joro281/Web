06476333914f92d1e1dc82cad22a46facee800ac
Joro <compengojt3223@gmail.com>
2025-07-11 20:53:47 +0800

    Initial Files

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import AboutWeb from "../components/ui/aboutweb";
+
+export default function About() {
+  return (
+    <>
+    <AboutWeb></AboutWeb>
+    </>
+  );
+}
---
+++ app/contact/page.tsx
@@ -0,0 +1,10 @@
+"use client";
+import ContactWeb from "../components/ui/contactweb";
+
+export default function Projects() {
+  return (
+    <>
+      <ContactWeb></ContactWeb>
+    </>
+  );
+}
